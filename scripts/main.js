dccafe7ec678d3686848cfd24c4cfd4abac3038a
Aliya Zagitova <ishmaeva@yandex.ru>
2022-03-15 22:41:49 +0800

    Создать проект Место

---
+++ scripts/main.js
@@ -0,0 +1,34 @@
+let popup = document.querySelector('.popup');
+let popup__opened = document.querySelector('.popup__opened');
+let editButton = document.querySelector('.profile__edit-button');
+let closeButton = document.querySelector('.popup__close-button');
+let addButton = document.querySelector('.profile__add-button');
+
+let formElement = popup.querySelector('.popup__form');
+const nameInput = popup.querySelector('.popup__input-name');
+const jobInput = popup.querySelector('.popup__input-info');
+const profileTitle = document.querySelector('.profile__title');
+const profileSubtitle = document.querySelector('.profile__subtitle');
+
+editButton.addEventListener('click', function() {
+  document.querySelector('.popup').style.display = 'block';
+  nameInput.value = profileTitle.textContent;
+  jobInput.value = profileSubtitle.textContent;
+  popup.classList.add('popup__opened');
+}
+)
+
+closeButton.addEventListener('click', function() {
+  document.querySelector('.popup').style.display = 'none';
+  popup.classList.remove('popup__opened');
+}
+)
+
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+  profileTitle.textContent = nameInput.value;
+  profileSubtitle.textContent = jobInput.value;
+  document.querySelector('.popup').style.display = 'none';
+}
+formElement.addEventListener('submit', formSubmitHandler);
+
